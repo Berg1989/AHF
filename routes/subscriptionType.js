7e7a6de1eb0218dbeac4e4ac663f152cc61ecd1d
Unknown <gelidusguy@gmail.com>
2019-04-29 12:25:14 +0200

    kontingenter kan oprettes!

--- routes/subscriptionType.js
+++ routes/subscriptionType.js
@@ -1,31 +1,45 @@
 const controller = require("../controllers/controller");
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 
 router
 .get('/', async (request, response) => {
-    response.render('subscriptionType');
+    response.locals.metaTags = {
+        title: 'Kontingenter',
+        description: 'Here goes the description',
+        keywords: 'Here goes keywords'
+    };
+    response.render('subscriptionType', {
+        layout: 'main',
+        success: request.session.success,
+        errors: request.session.errors,
+        goodErrors: request.session.goodErrors,
+    });
+    request.session.errors = null;
+    request.session.goodErrors = null;
 })
-//.get('/subscriptionTypes', (request,response) =>{
-  //  controller.getSubTypes().then(result =>{
-    //    response.send(result);
-    //});
-//})
 
 .post('/', async (request,response) =>{
     const {name, duration, mdrPrice} = request.body;
     const exists = await controller.findSubType(name);
+    const errors = validationResult(request);
+    const goodErrors = validationResult(request);
     if(!exists){
         const result = await controller.createSubType(name, duration, mdrPrice)
         if(result){
+            request.session.goodErrors = [{ message: 'Kontingent oprettet' }];
             request.session.subType = result;
             request.session.success = true;
             response.redirect('/subscriptionType');
         }
     }else{
+        request.session.errors = [{ msg: 'Kontingent eksisterer allerede' }];
+        request.session.name = request.body.name;
         request.session.success = false;
-        request.redirect('/subscriptionType');
+        response.redirect('/subscriptionType');
+        
     }
 });
 
