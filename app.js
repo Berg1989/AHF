c9b78f1c9fa3b9d9672d8d7f25abda68ec14fbab
Unknown <gelidusguy@gmail.com>
2019-04-29 09:58:05 +0200

    nogle ting stod to gange

--- app.js
+++ app.js
@@ -42,8 +42,6 @@ const loginRouter = require('./routes/login');
 app.use('/login', loginRouter);
 const subscriptionTypeRouter = require('./routes/subscriptionType');
 app.use('/subscriptionType', subscriptionTypeRouter);
-const subscriptionTypeRouter = require('./routes/subscriptionType');
-app.use('/subscriptionType', subscriptionTypeRouter);
 const userProfiles = require('./routes/userprofiles');
 app.use('/userprofiles', userProfiles);
 
