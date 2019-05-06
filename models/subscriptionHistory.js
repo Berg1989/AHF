db914556a98e4e39959f0af051e276cc3426224a
RasmusTraeholt <traeholt@live.dk>
2019-05-06 15:04:25 +0200

    a

--- models/subscriptionHistory.js
+++ models/subscriptionHistory.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const subscriptionHistory = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'user' },
-    subscription: { type: Schema.Types.ObjectId, ref: 'subscription' }
+    subscriptions: [{ type: Schema.Types.ObjectId, ref: 'subscription' }]
 });
 
 module.exports = mongoose.model('subscriptionHistory', subscriptionHistory);
