10d476774d996a522b0d3e81a747855b79194731
MortenF <morten@faber-lyd.dk>
2019-05-07 12:14:31 +0200

    HTML til personlige events

---
+++ routes/public/events.js
@@ -0,0 +1,11 @@
+const controller = require("../../controllers/controller");
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+router
+
+
+
+module.exports = router;
