363b6eaed974e6290dcb6dfbf19ebd9c3aa41d90
Kennet Najbjerg <99kennetn@gmail.com>
2019-05-07 09:03:28 +0200

    testet adminTest

--- test/adminTest.js
+++ test/adminTest.js
@@ -6,8 +6,8 @@ const request = require('supertest');
 //TC_1
 describe('reset password test', function () {
     it('resetPassword() test', async function () {
-        const data = {email: "fs@test.dk", password: "kode", firstname: "Lars", lastname: "Larsen", title: "Hej", level: 3, func: "Test"};
-        const user = await controller.createUser(data.email, data.password, data.firstname, data.lastname, data.title, data.level, data.func);
+        const data = {email: "fs@test.dk", password: "kode", firstname: "Lars", lastname: "Larsen"};
+        const user = await controller.createUser(data.email, data.password, data.firstname, data.lastname);
         await controller.resetPassword(user.id);
         assert.notEqual(data.password, user.password, 'Success, new password created');
         //clean up
@@ -18,7 +18,7 @@ describe('reset password test', function () {
 //TC_2
 describe('delete user test', function () {
     it('deleteUser() test', async function () {
-        const user = await controller.createUser("borge@gmail.com", "12345", "Børge", "Jensen", "member", 3, "testcase");
+        const user = await controller.createUser("borge@gmail.com", "12345", "Børge", "Jensen");
         const result = await controller.deleteUser(user._id);
         assert.exists(result, 'Exists');
     })
