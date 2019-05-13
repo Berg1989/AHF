8cbbdb908e77d40bf6a5f7c0199bfe607050e7f1
MortenF <morten@faber-lyd.dk>
2019-05-13 13:32:29 +0200

    Test in progress

--- test/eventTest.js
+++ test/eventTest.js
@@ -5,8 +5,17 @@ const request = require('supertest');
 
 describe('eventSignUp test', function () {
     it('eventSignUp() test', async function () {
-        const data = { email: 'hej@gmail.com', password: '12345', firstname: 'Hans', lastname: 'Hansen'};
-        const user = controller.createUser(data.email, data.password, data.firstname, data.lastname);
+        const userData = { email: 'hej@gmail.com', password: '12345', firstname: 'Hans', lastname: 'Hansen', usertype: '5cd04ba11c9d4400009071cd'};
+        const eventData = { headline: 'DanskeJevle', author: 'Danmark', startdate: '2019-05-06', enddate: '2019-07-11', body: 'Hejsa' , deadline: '2019-05-09', maxparticipants: 11, price: 1005};
         
+        const event = controller.createEvent(eventData.headline, eventData.author, eventData.startdate, eventData.enddate, eventData.body, eventData.deadline, eventData.price)
+        const user = controller.createUser(userData.email, userData.password, userData.firstname, userData.lastname, userData.usertype);
+        
+        const result = await controller.eventSignUp(event.id, user.id);
+
+        assert.include(event, user)
+
+        await controller.deleteUser(user);
+        await controller.deleteEvent(event);
     })
 })
